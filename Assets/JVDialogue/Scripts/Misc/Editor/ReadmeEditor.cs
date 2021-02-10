f99a7f684ba20d346acd4d224b583c5b194cd8aa
Jay <s199064@students.aie.edu>
2021-02-10 12:26:23 -0800

    readme WIP and more package friendly file ordering

---
+++ Assets/JVDialogue/Scripts/Misc/Editor/ReadmeEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+
+namespace JVDialogue
+{
+    [CustomEditor(typeof(Readme))]
+    public class ReadmeEditor : Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+        }
+    }
+}
---
+++ Assets/JVDialogue/Scripts/Misc/Readme.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace JVDialogue
+{
+    public class Readme : ScriptableObject
+    {
+        public string title;
+        public Texture2D icon;
+
+        public string[] headers;
+        public string[] body;
+        public bool[] completedSection;
+    }
+}
